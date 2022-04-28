578536082761e6f72cf2de5d2139a1f8ddaa0c23
Christian <Chrisgalera1555@gmail.com>
2022-04-27 21:01:18 -0300

    parking spot dto

--- src/main/java/com/api/parkingcontrol/controllers/ParkingSpotController.java
+++ src/main/java/com/api/parkingcontrol/controllers/ParkingSpotController.java
@@ -10,7 +10,7 @@ import org.springframework.web.bind.annotation.RestController;
 @CrossOrigin(origins = "*", maxAge = 3600)
 @RequestMapping("/parking-spot")
 public class ParkingSpotController {
-    
+
     @Autowired
     ParkingSpotService parkingSpotService;
 
